704ed68cf75243869c45eec71ea13810f4bc6a98
v-misho-tektumanidze <v.misho.tektumanidze@reddit.com>
2026-01-05 15:15:55 +0400

    Adds prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-  ]
+  ],
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+  }
 })
