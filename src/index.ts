ec2bebf10d2f6df5e22371c8e5f521f8a4c1dd45
v-misho-tektumanidze <v.misho.tektumanidze@reddit.com>
2026-01-05 15:27:14 +0400

    Adds husky package

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,41 @@
-export function onSubmit(e: SubmitEvent, formEl: HTMLFormElement, tableEl: HTMLTableElement) {
+export function onSubmit(
+  e: SubmitEvent,
+  formEl: HTMLFormElement,
+  tableEl: HTMLTableElement,
+) {
   e.preventDefault();
 
   const formData = new FormData(formEl);
-  const category = formData.get('category');
-  const amount = formData.get('amount');
+  const category = formData.get("category");
+  const amount = formData.get("amount");
 
-  const tableRowEl = document.createElement('tr');
-  tableRowEl.className = 'border-t-solid border-t-1 border-t-neutral-600';
+  const tableRowEl = document.createElement("tr");
+  tableRowEl.className = "border-t-solid border-t-1 border-t-neutral-600";
 
-  const categoryTableCellEl = document.createElement('td');
+  const categoryTableCellEl = document.createElement("td");
   categoryTableCellEl.innerText = String(category);
-  categoryTableCellEl.className = 'border-r-solid border-r-1 border-r-neutral-600';
+  categoryTableCellEl.className =
+    "border-r-solid border-r-1 border-r-neutral-600";
 
-  const amountCellEl = document.createElement('td');
+  const amountCellEl = document.createElement("td");
   amountCellEl.innerText = `${amount}$`;
 
   tableRowEl.appendChild(categoryTableCellEl);
   tableRowEl.appendChild(amountCellEl);
+
   tableEl.appendChild(tableRowEl);
 
   formEl.reset();
 }
 
 function main() {
-  const formEl = document.querySelector<HTMLFormElement>('#form');
-  const tableEl = document.querySelector<HTMLTableElement>('#table');
+  const formEl = document.querySelector<HTMLFormElement>("#form");
+  const tableEl = document.querySelector<HTMLTableElement>("#table");
 
   if (formEl && tableEl) {
-    formEl.addEventListener('submit', e => onSubmit(e, formEl, tableEl));
+    formEl.addEventListener("submit", (e) => onSubmit(e, formEl, tableEl));
   } else {
-    console.error('Form not found.');
+    console.error("Form not found.");
   }
 }
 
