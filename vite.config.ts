cc09b301378bf0f2e2602585032f2df841c45b14
Misho Tektumanidze <mishotektumanidze@gmail.com>
2026-01-05 11:31:54 +0000

    Add prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ]
-})
+  plugins: [tailwindcss()],
+  root: resolve(__dirname, "src"),
+  build: {
+    outDir: resolve(__dirname, "dist"),
+  },
+});
